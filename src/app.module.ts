7867857cdde7895f4065f46dbfef1d7bf1a24a32
Wonjong Oh <jgyuity1289@gmail.com>
2023-11-28 02:05:57 +0900

    fix(wip): TypeORM setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,32 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
-import { join } from 'path';
 import { RestaurantsModule } from './restaurants/restaurants.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [GraphQLModule.forRoot<ApolloDriverConfig>({
-    driver: ApolloDriver,
-    autoSchemaFile: true,
-  }), RestaurantsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+    }),
+    RestaurantsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'wonjong',
+      password: '1234',
+      database: 'nuber-eats',
+      synchronize: true,
+      logging: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,16 +1,18 @@
-import {Args, Mutation, Query, Resolver } from "@nestjs/graphql";
-import { Restaurant } from "./entities/restaurant.entity";
-import { CreateRestaurantDto } from "./dtos/create-restaurant.dto";
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Restaurant } from './entities/restaurant.entity';
+import { CreateRestaurantDto } from './dtos/create-restaurant.dto';
 
 @Resolver()
 export class RestaurantsResolver {
-    @Query(returns => [Restaurant])
-    restaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
-        return [];
-    }
+  @Query((returns) => [Restaurant])
+  restaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
+    return [];
+  }
 
-    @Mutation(returns => Boolean)
-    createRestaurant(@Args() createRestaurantInput: CreateRestaurantDto): boolean {
-        return true;
-    }
+  @Mutation((returns) => Boolean)
+  createRestaurant(
+    @Args() createRestaurantInput: CreateRestaurantDto,
+  ): boolean {
+    return true;
+  }
 }
